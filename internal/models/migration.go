603cbdb835f523c55afd99253b3e358739c83cc8
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-04 11:11:46 +0100

    feat: add more comments to migration model

--- internal/models/migration.go
+++ internal/models/migration.go
@@ -2,7 +2,10 @@ package models
 
 // Migration represents a migration script, which is a file that contains SQL.
 type Migration struct {
-	Name    string
-	Path    string
+	// TableName is the name of the table that migration is going to be applied to.
+	TableName string
+	// Path is the path to the migration file.
+	Path string
+	// Version is the version of the migration.
 	Version int
 }
