0c735b65503691a7311c7e6c0a3bd5abee7a8ce8
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-05 18:09:47 +0100

    feat: add constructor and runner for cli

--- cli/cli.go
+++ cli/cli.go
@@ -9,11 +9,28 @@ import (
 )
 
 type Migrator interface {
-	MigrateUp(ctx context.Context, migration *models.Migration) error
-	MigrateDown(ctx context.Context, migration *models.Migration) error
+	MigrateUp(ctx context.Context, migrations []models.Migration) error
+	MigrateDown(ctx context.Context, migrations []models.Migration) error
 }
 
 type CLI struct {
 	logger   *zap.Logger
 	migrator Migrator
+}
+
+func New(logger *zap.Logger, migrator Migrator) *CLI {
+	return &CLI{logger: logger, migrator: migrator}
+}
+
+func (c *CLI) Run(ctx context.Context, cmd models.Command, migrations []models.Migration) error {
+	c.logger.Info("running command", zap.String("command", cmd.Type.String()))
+
+	switch cmd.Type {
+	case models.CommandMigrateUp:
+		return c.migrator.MigrateUp(ctx, migrations)
+	case models.CommandMigrateDown:
+		return c.migrator.MigrateDown(ctx, migrations)
+	default:
+		return nil
+	}
 }
