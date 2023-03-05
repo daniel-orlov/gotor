30394a8979984ba4ed175c4e8abe37b92cc4f530
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-05 18:54:06 +0100

    feat: update service stub to match the interface definition

--- internal/service/migrator/service.go
+++ internal/service/migrator/service.go
@@ -16,13 +16,13 @@ func NewService(logger *zap.Logger) *Service {
 	return &Service{logger: logger}
 }
 
-func (s *Service) MigrateUp(ctx context.Context, migration *models.Migration) error {
+func (s *Service) MigrateUp(ctx context.Context, migrations []models.Migration) error {
 	s.logger.Info("migrating up")
 
 	return nil
 }
 
-func (s *Service) MigrateDown(ctx context.Context, migration *models.Migration) error {
+func (s *Service) MigrateDown(ctx context.Context, migrations []models.Migration) error {
 	s.logger.Info("migrating down")
 
 	return nil
